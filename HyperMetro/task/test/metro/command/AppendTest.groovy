bd30cfb6db7679d61d6f6fe9807a4d848e5ab48b
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-10-31 18:14:50 +0200

    Updated tests

--- HyperMetro/task/test/metro/command/AppendTest.groovy
+++ HyperMetro/task/test/metro/command/AppendTest.groovy
@@ -1,10 +1,8 @@
 package metro.command
 
-import org.junit.Test
 import spock.lang.Specification
 
 class AppendTest extends Specification {
-    @Test
     def "Test"() {
         given:
         def command = new Append(null)
