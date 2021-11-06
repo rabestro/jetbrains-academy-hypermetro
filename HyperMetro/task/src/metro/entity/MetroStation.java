20a767f12ff6e6d84abb7df45a2395ad33391abd
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-06 12:31:09 +0200

    Add method MetroStation::from

--- HyperMetro/task/src/metro/entity/MetroStation.java
+++ HyperMetro/task/src/metro/entity/MetroStation.java
@@ -1,8 +1,14 @@
 package metro.entity;
 
+import com.google.gson.JsonObject;
 import lombok.Data;
 
 @Data
 public class MetroStation {
     private final String name;
+
+    static MetroStation from(final JsonObject jsonStation) {
+        final var name = jsonStation.get("name").getAsString();
+        return new MetroStation(name);
+    }
 }
