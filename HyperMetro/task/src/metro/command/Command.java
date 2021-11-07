b68468180962f64388f283c3e7017112d764d75a
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-07 16:33:58 +0200

    Updated Command

--- HyperMetro/task/src/metro/command/Command.java
+++ HyperMetro/task/src/metro/command/Command.java
@@ -1,25 +1,8 @@
 package metro.command;
 
-import metro.model.MetroMap;
-
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.regex.Pattern;
+import java.util.function.Function;
 
-public interface Command extends BiConsumer<MetroMap, List<String>> {
-    Pattern CAMEL_CASE = Pattern.compile("(\\p{Lower})(\\p{Upper})");
-
-    default String name() {
-        return CAMEL_CASE
-                .matcher(this.getClass().getSimpleName())
-                .replaceAll("$1-$2")
-                .toLowerCase();
-    }
-
-    static void checkParametersNumber(final List<String> parameters, final int number) {
-        if (parameters.size() != number) {
-            throw new IllegalArgumentException(
-                    "Invalid number of parameters. Expected: " + number + ", actual: " + parameters.size());
-        }
-    }
+public interface Command extends Function<List<String>, String> {
+    String name();
 }
