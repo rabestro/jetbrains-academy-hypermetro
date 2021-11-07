0761d012c48e442c4649c7fb2d81bae39853bf66
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-07 16:58:29 +0200

    Add Append command

---
+++ HyperMetro/task/src/metro/command/Append.java
@@ -0,0 +1,19 @@
+package metro.command;
+
+import metro.service.MetroService;
+
+import java.util.List;
+
+public class Append extends HyperMetroCommand{
+
+    public Append(final MetroService metroService) {
+        super(metroService);
+    }
+
+    @Override
+    public String apply(final List<String> parameters) {
+        validateParametersNumber(parameters, 2);
+        metroService.append(parameters.get(0), parameters.get(1));
+        return "Metro station successfully added";
+    }
+}
