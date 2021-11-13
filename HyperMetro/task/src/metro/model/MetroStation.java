f260b0605e63767d34068a8e8d159a61b1c1acb0
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-13 22:34:35 +0200

    Add lombok for MetroStation and StationID

--- HyperMetro/task/src/metro/model/MetroStation.java
+++ HyperMetro/task/src/metro/model/MetroStation.java
@@ -1,62 +1,14 @@
 package metro.model;
 
+import lombok.Data;
+
 import java.util.Set;
 
+@Data
 public class MetroStation {
     private final StationID stationID;
     private Set<StationID> transfer = Set.of();
     private Set<StationID> next = Set.of();
     private Set<StationID> prev = Set.of();
     private int time;
-
-    public MetroStation(final StationID stationID) {
-        this.stationID = stationID;
-    }
-
-    public StationID getStationID() {
-        return stationID;
-    }
-
-    public Set<StationID> getTransfer() {
-        return transfer;
-    }
-
-    public void setTransfer(final Set<StationID> transfer) {
-        this.transfer = transfer;
-    }
-
-    public Set<StationID> getNext() {
-        return next;
-    }
-
-    public void setNext(final Set<StationID> next) {
-        this.next = next;
-    }
-
-    public Set<StationID> getPrev() {
-        return prev;
-    }
-
-    public void setPrev(final Set<StationID> prev) {
-        this.prev = prev;
-    }
-
-    public int getTime() {
-        return time;
-    }
-
-    public void setTime(final int time) {
-        this.time = time;
-    }
-
-    @Override
-    public String toString() {
-        return "MetroStation{" +
-                "stationID=" + stationID +
-                ", transfer=" + transfer.toString() +
-                ", next=" + next.toString() +
-                ", prev=" + prev.toString() +
-                ", time=" + time +
-                '}';
-    }
 }
--- HyperMetro/task/src/metro/model/StationID.java
+++ HyperMetro/task/src/metro/model/StationID.java
@@ -1,34 +1,9 @@
 package metro.model;
 
-import java.util.Objects;
+import lombok.Value;
 
+@Value
 public class StationID {
-    private final String line;
-    private final String name;
-
-    public StationID(final String line, final String name) {
-        this.line = line;
-        this.name = name;
-    }
-
-    public String getLine() {
-        return line;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final var stationID = (StationID) o;
-        return line.equals(stationID.line) && name.equals(stationID.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(line, name);
-    }
+    String line;
+    String name;
 }
