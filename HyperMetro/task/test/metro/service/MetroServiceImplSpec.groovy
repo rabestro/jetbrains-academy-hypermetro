c33222a73739c8c51c013a99332f61a08b33b034
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-24 21:58:58 +0200

    Add Spock 2 test

--- HyperMetro/task/test/metro/service/MetroServiceImplSpec.groovy
+++ HyperMetro/task/test/metro/service/MetroServiceImplSpec.groovy
@@ -1,10 +1,28 @@
 package metro.service
 
+import metro.model.MetroLine
+import metro.repository.MetroRepository
 import spock.lang.Specification
+import spock.lang.Subject
 
 class MetroServiceImplSpec extends Specification {
-    def "GetMetroStation"() {
-        expect: 'My own unit test'
-        2 + 2 == 5
+    def repository = Mock MetroRepository
+    def lineMock = Mock MetroLine
+
+    @Subject
+    def service = new MetroServiceImpl(repository)
+
+    def 'should return metro line'() {
+        given:
+        def lineName = 'First line'
+
+        when:
+        def metroLine = service.getMetroLine(lineName)
+
+        then:
+        1 * repository.getLine(lineName) >> Optional.of(lineMock)
+        and:
+        metroLine === lineMock
     }
+
 }
