a564c5f878f9c2074652a774e79de54c9df30844
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-24 19:57:58 +0200

    Add MetroServiceImplTest

---
+++ HyperMetro/task/test/metro/service/MetroServiceImplTest.java
@@ -0,0 +1,13 @@
+package metro.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MetroServiceImplTest {
+
+    @Test
+    public void getMetroLine() {
+        assertTrue("Proof of concept. User defined unit test.", true);
+    }
+}
