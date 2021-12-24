af8f29d585db8de4a6a4a38969a4f5e968d80028
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-24 21:30:44 +0200

    Add Spock 1.3 dependency

---
+++ HyperMetro/task/test/metro/service/MetroServiceImplSpec.groovy
@@ -0,0 +1,10 @@
+package metro.service
+
+import spock.lang.Specification
+
+class MetroServiceImplSpec extends Specification {
+    def "GetMetroStation"() {
+        expect: 'My own unit test'
+        2 + 2 == 5
+    }
+}
