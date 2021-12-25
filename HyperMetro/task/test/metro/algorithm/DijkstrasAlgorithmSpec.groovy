b0cb051dec7cc1aa6c579c145a8ea74714afcf9c
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-25 23:56:05 +0200

    Add one more test

--- HyperMetro/task/test/metro/algorithm/DijkstrasAlgorithmSpec.groovy
+++ HyperMetro/task/test/metro/algorithm/DijkstrasAlgorithmSpec.groovy
@@ -1,11 +1,14 @@
 package metro.algorithm
 
 import spock.lang.Specification
+import spock.lang.Subject
 import spock.lang.Unroll
 
 class DijkstrasAlgorithmSpec extends Specification {
 
-    static final GRAPH = [
+    static final SAMPLE_ONE = [A: [B: 7, C: 2], B: [A: 3, C: 5], C: [A: 1, B: 3]]
+
+    static final SAMPLE_TWO = [
             A: [B: 5, H: 2],
             B: [A: 5, C: 7],
             C: [B: 7, D: 3, G: 4],
@@ -16,11 +19,40 @@ class DijkstrasAlgorithmSpec extends Specification {
             H: [G: 3]
     ]
 
+    @Subject
+    def algorithm = new DijkstrasAlgorithm()
+
+    @Unroll("from #sid to #tid the time is #time and the path is #expected")
+    def 'should find a route for sample one'() {
+        given:
+        def graph = createGraph SAMPLE_ONE
+
+        and:
+        def source = graph[sid]
+        def target = graph[tid]
+
+        when:
+        def route = algorithm.findRoute(source, target)
+
+        then:
+        with(route) {
+            it.id == expected
+            it.last.distance == time
+        }
+
+        where:
+        sid | tid || time | expected
+        'A' | 'A' || 0    | ['A']
+        'B' | 'B' || 0    | ['B']
+        'A' | 'B' || 5    | ['A', 'C', 'B']
+        'B' | 'A' || 3    | ['B', 'A']
+        'B' | 'C' || 5    | ['B', 'C']
+        'C' | 'A' || 1    | ['C', 'A']
+
+    }
+
     @Unroll("from #sourceId to #targetId the time is #time and the path is #expected")
     def "should find a route"() {
-        given:
-        def algorithm = new DijkstrasAlgorithm()
-
         when:
         def result = algorithm.findRoute(source, target)
         def route = result.id
@@ -42,9 +74,12 @@ class DijkstrasAlgorithmSpec extends Specification {
         'A'      | 'G'      || 5    | ['A', 'H', 'G']
         'C'      | 'D'      || 3    | ['C', 'D']
         'D'      | 'C'      || 19   | ['D', 'E', 'F', 'G', 'C']
+        'B'      | 'D'      || 10   | ['B', 'C', 'D']
+        'D'      | 'B'      || 26   | ['D', 'E', 'F', 'G', 'C', 'B']
+        'D'      | 'H'      || 33   | ['D', 'E', 'F', 'G', 'C', 'B', 'A', 'H']
 
         and:
-        graph = createGraph(GRAPH)
+        graph = createGraph SAMPLE_TWO
 
         and:
         source = graph[sourceId]
