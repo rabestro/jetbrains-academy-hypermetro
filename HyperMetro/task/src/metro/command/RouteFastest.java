b632fc8001236d0ac6209fa2b155c77ebf13171c
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-28 15:44:00 +0200

    Fixed java doc

--- HyperMetro/task/src/metro/command/RouteFastest.java
+++ HyperMetro/task/src/metro/command/RouteFastest.java
@@ -10,8 +10,9 @@ import java.util.List;
 /**
  * Prints the fastest way by using Dijkstra's algorithm.
  * The travel time between station is taken into account.
- * <p>
  * Transferring from one line to another takes 5 minutes.
+ * <p>
+ * The program prints the estimate total travel time.
  */
 public class RouteFastest extends RouteCommand {
     private static final SearchAlgorithm<StationId> algorithm = new DijkstrasSearchAlgorithm<>();
