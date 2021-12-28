ff590806c05a9df20bb694a67b442d9ce8c37dcf
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-28 21:24:57 +0200

    Add images

--- HyperMetro/task/test/metro/algorithm/GraphSpec.groovy
+++ HyperMetro/task/test/metro/algorithm/GraphSpec.groovy
@@ -4,7 +4,7 @@ import spock.lang.Specification
 
 class GraphSpec extends Specification {
     def "should return edges for a given node"() {
-        given:
+        given: 'a simple graph with three nodes'
         def graph = new Graph([
                 A: [B: 7, C: 2],
                 B: [A: 3, C: 5],
