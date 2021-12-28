f01b3086aca4d18a8e78c10e4c42118702d9a84d
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-29 01:01:52 +0200

    Add MetroRepositoryImpSpec

---
+++ HyperMetro/task/test/metro/repository/MetroRepositoryImpSpec.groovy
@@ -0,0 +1,37 @@
+package metro.repository
+
+import metro.model.MetroLine
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Subject
+
+class MetroRepositoryImpSpec extends Specification {
+
+    @Shared
+    def lineOne = Mock MetroLine
+    @Shared
+    def lineTwo = Mock MetroLine
+
+    @Subject
+    def repository = new MetroRepositoryImpl()
+
+    def "should find metro line by name"() {
+        given: 'metro map with two lines'
+        repository.metroMap = [one: (lineOne), two: (lineTwo)]
+
+        when: 'we request the repository for existing line'
+        def metroLine = repository.getLine(lineName)
+
+        then: 'repository returns metro line'
+        metroLine.isPresent()
+
+        and: 'the metro line is expected'
+        metroLine.get() === expected
+
+        where:
+        lineName | expected
+        'one'    | lineOne
+        'two'    | lineTwo
+    }
+
+}
