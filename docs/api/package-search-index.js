2662c5c2b87a918b0bd7e48c7e5ccd380ceac065
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-31 00:59:28 +0200

    Added JavaDoc

---
+++ docs/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"metro"},{"l":"metro.algorithm"},{"l":"metro.command"},{"l":"metro.config"},{"l":"metro.controller"},{"l":"metro.model"},{"l":"metro.repository"},{"l":"metro.service"},{"l":"metro.ui"}];updateSearchResults();
